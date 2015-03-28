e5c1b347894a204bc80c7f591b248c5ef287c00c
andyjefferson <andy@datanucleus.org>
2015-03-28 13:33:03 +0000

    javadocs

--- src/main/java/org/datanucleus/api/rest/RestServlet.java
+++ src/main/java/org/datanucleus/api/rest/RestServlet.java
@@ -10,10 +10,10 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License. 
- 
+limitations under the License.
 
 Contributors:
+2013-2015 Andy Jefferson - updated to use persistence.xml, GZIP, JDOQL, JPQL, FetchGroups
     ...
  **********************************************************************/
 package org.datanucleus.api.rest;
